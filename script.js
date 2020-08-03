907446454c89e367e6134647f8a76f71b7789462
ing-arriola <ingeniero.arriola@gmail.com>
2020-08-03 16:20:19 -0600

    Added functions to show spinner and also to remove the spinner once the API send a quote

--- script.js
+++ script.js
@@ -3,14 +3,29 @@ const quoteText = document.getElementById("quoteText");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("newQuote");
+const spinner = document.getElementById("loader");
+
+const loading = () => {
+  spinner.hidden = false;
+  quoteContainer.hidden = true;
+};
+
+const loaded = () => {
+  if (!spinner.hidden) {
+    spinner.hidden = true;
+    quoteContainer.hidden = false;
+  }
+};
 
 const getQuote = async () => {
+  loading();
   const proxyUrl = "https://secure-gorge-04157.herokuapp.com/";
   const apiUrl =
     "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
   try {
     const res = await fetch(proxyUrl + apiUrl);
     const data = await res.json();
+
     //If a quote is too long then modify its size
     if (data.quoteText.length > 50) {
       quoteText.classList.add("long-quote");
@@ -24,6 +39,7 @@ const getQuote = async () => {
     } else {
       authorText.innerText = data.quoteAuthor;
     }
+    loaded();
   } catch (err) {
     console.log("whoops there is no quote", err);
     getQuote();
